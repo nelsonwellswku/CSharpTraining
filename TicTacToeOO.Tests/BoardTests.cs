81f35369c78011fe0f045b253fe1ce465a8afd46
Nelson Wells <nelsonwellswku@gmail.com>
2012-09-18 22:24:32 -0500

    Board unit tests

---
+++ TicTacToeOO.Tests/BoardTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TicTacToeOO;
+
+namespace TicTacToeOO.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void ModifyBoard_ModifyFiveWithO_Success()
+        {
+            Board board = new Board();
+            Assert.AreEqual<bool>(true, board.ModifyBoard(5, "O"));
+        }
+
+        [TestMethod]
+        public void CheckForWinner_NoWinner_ReturnFalse()
+        {
+            Board board = new Board();
+            Assert.AreEqual<bool>(false, board.CheckForWinner());
+        }
+
+        [TestMethod]
+        public void CheckForWinner_TopRowWinner_Success()
+        {
+            Board board = new Board();
+            board.ModifyBoard(1, "X");
+            board.ModifyBoard(2, "X");
+            board.ModifyBoard(3, "X");
+
+            Assert.AreEqual<bool>(true, board.CheckForWinner());
+        }
+    }
+}
